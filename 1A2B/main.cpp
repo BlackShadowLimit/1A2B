60c170badc102166cdaf87ec20f25be55e23175e
Arthur <Arthur952006@gmail.com>
2022-04-03 17:07:34 +0800

    Initial Commit

---
+++ 1A2B/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  1A2B
+//
+//  Created by Arthur on 2022/4/3.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
